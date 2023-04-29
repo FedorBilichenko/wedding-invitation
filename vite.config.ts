52537900c50571f45171d7195b5a514acdffcd21
Федор Биличенко <f.bilichenko@ktsstudio.ru>
2023-04-29 15:59:01 +0300

    правки

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import preload from "vite-plugin-preload";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), preload()],
   resolve: {
     alias: {
       styles: path.resolve(__dirname, './src/styles'),
